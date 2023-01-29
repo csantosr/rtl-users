103a1918b972c94e9b5998d5d5c87df7ba68bf9f
Cristian Santos <csantosr@unal.edu.co>
2023-01-29 16:14:21 -0500

    Testing app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("It can receive a new user and show it on a list", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  const button = screen.getByRole("button");
+
+  user.click(button);
+
+  const nameCell = screen.getByRole("cell", { name: "jane" });
+  expect(nameCell).toBeInTheDocument();
+
+  const emailCell = screen.getByRole("cell", { name: "jane@jane.com" });
+  expect(emailCell).toBeInTheDocument();
 });
